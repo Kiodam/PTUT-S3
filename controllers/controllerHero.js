b0ceb864e18a6b5303c58ce3595014728ee1505a
Kiodam <paul.wust3@gmail.com>
2021-12-20 22:44:14 +0100

    debug controllerHero

--- controllers/controllerHero.js
+++ controllers/controllerHero.js
@@ -1,39 +1,42 @@
-export default class ControllerHero {
-    constructor(hero, controllerMap) {
+export default class ControllerHero{
+    constructor(hero,controllerMap) {
         this.actions = [];
-        document.getElementById('gauche').addEventListener('click', () => {
+        document.getElementById('gauche').addEventListener('click',()=>{
             let div = document.createElement('div');
             this.actions.push("gauche");
             div.className = "action"
             div.innerHTML += "Gauche" + "<br />";
             document.getElementById('actions').appendChild(div);
+            console.log("gauche")
         });
-        document.getElementById('avancer').addEventListener('click', () => {
+        document.getElementById('avancer').addEventListener('click',()=>{
             let div = document.createElement('div');
             this.actions.push("avancer")
             div.className = "action"
             div.innerHTML += "Avancer" + "<br />";
             document.getElementById('actions').appendChild(div);
+            console.log("avancer")
         });
-        document.getElementById('droite').addEventListener('click', () => {
+        document.getElementById('droite').addEventListener('click',()=>{
             let div = document.createElement('div');
             this.actions.push("droite");
             div.className = "action"
             div.innerHTML += "Droite" + "<br />";
             document.getElementById('actions').appendChild(div);
+            console.log("droite")
         });
-        document.getElementById('execute').addEventListener('click', async () => {
+        document.getElementById('execute').addEventListener('click',async()=>{
             let div = document.createElement('div');
             await hero.execute(this.actions);
             controllerMap.checkWin();
             div.innerHTML = "";
-            this.actions.splice(0, this.actions.length);
+            this.actions.splice(0,this.actions.length);
         });
-        document.getElementById('clear').addEventListener('click', async () => {
-            this.actions.splice(0, this.actions.length)
+        document.getElementById('clear').addEventListener('click',async()=>{
+            this.actions.splice(0,this.actions.length)
             let tab = document.getElementsByClassName("action")
             let size = tab.length
-            for (let i = size - 1; i >= 0; i--) {
+            for (let i = size-1; i >= 0; i--) {
                 tab[i].remove();
             }
         });
